83afa1dfb025b304286d8a0f1c4dec0b40c249ca
imorenofern <imorenofern@alumni.unav.es>
2025-03-13 15:23:33 +0100

    Update factorial.m

--- factorial.m
+++ factorial.m
@@ -1,16 +1,16 @@
 clear all
 clc
 
-num=input('Introduzca un número entero:);
+num=input("Introduzca un número entero:");
 
-factorial=1
+factorial=1;
 
 for i=1:num-1
-    factorial=factorial*i
+    factorial=factorial*i;
 end
 
 if num>=0
-    disp('El factorial de' num2str(num) 'es:' factorial)
+    disp(['El factorial de ' num2str(num) ' es:' num2str(factorial)]);
 else
-    disp(['El factorial de',num,'es:',factorial])
+    disp("Introduzca un valor mayor a 0");
 end
